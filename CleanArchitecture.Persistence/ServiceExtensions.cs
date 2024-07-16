d8fe72fcc1380546cd6cc2beb9d14c5910611fe3
Antônio Torres Magalhães <91563143+torresmagalhaes@users.noreply.github.com>
2024-07-15 21:56:01 -0300

    Add files via upload

---
+++ CleanArchitecture.Persistence/ServiceExtensions.cs
@@ -0,0 +1,21 @@
+﻿using CleanArchitecture.Domain.Interfaces;
+using CleanArchitecture.Persistence.Context;
+using CleanArchitecture.Persistence.Repositories;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace CleanArchitecture.Persistence;
+
+public static class ServiceExtensions
+{
+    public static void ConfigurePersistenceApp(this IServiceCollection services,
+                                                IConfiguration configuration)
+    {
+        var connectionString = configuration.GetConnectionString("Npgsql");
+        services.AddDbContext<AppDbContext>(opt => opt.UseNpgsql(connectionString));
+
+        services.AddScoped<IUnitOfWork, UnitOfWork>();
+        services.AddScoped<IUserRepository, UserRepository>();
+    }
+}
